6c4c77e33665cedf47b0ef3c998185f918b55f0c
tiohs <hamiltonsilva551@gmail.com>
2021-09-19 14:13:52 +0100

    index feito

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-const carroselLeft = document.querySelector('')
+var carroselLeft = document.querySelector('#carousel2');
+
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false,
+  cycle : right
+})
